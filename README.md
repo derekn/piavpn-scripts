45618b6e8a242da878b55f02b676036391286df6
Derek Nicol <1420397+derekn@users.noreply.github.com>
2022-02-28 13:27:45 -0600

    adding port_forwarding script

--- README.md
+++ README.md
@@ -16,11 +16,20 @@ _Note: these are unofficial scripts, not affiliated with Private Internet Access
 
 ### Installation/Quick-Start
 
+The quick-start scripts runs the other scripts in order to connect to the Wireguard VPN server.  
+To enable port-forwarding, you must run the `port_forwarding.sh` script manually after connecting.
+
 ```sh
 git pull https://github.com/derekn/piavpn-scripts.git
 cd piavpn-scripts
 
 PIA_USER=user PIA_PASS=pass ./setup.sh
+
+# VPN should now be connecting if there were no errors.
+# interface name is "pia", and can be checked using `wg show pia`
+
+# optionally enable port-forwarding
+./port_forwarding.sh
 ```
 
 ### Manual Usage
@@ -31,6 +40,7 @@ Scripts should be run in the following order, exporting the output environment v
 1. get_region - _outputs `WG_SERVER_IP` and `WG_HOSTNAME`_
 1. get_token - _outputs `PIA_TOKEN`_
 1. connect_wireguard
+1. port_forwarding - _optional, outputs `PAYLOAD_AND_SIGNATURE`, `PORT_FORWARD_PORT` and `EXPIRES_AT`_
 
 ### Included Scripts
 
@@ -40,4 +50,5 @@ Scripts should be run in the following order, exporting the output environment v
 | [get_region.sh](get_region.sh) | | Get region details.<br>Optional, `PREFERRED_REGION` to set specific region by id (ex. ca_toronto). `PIA_PF=true` to only select regions supporting port-forwarding. |
 | [get_token.sh](get_token.sh) | `PIA_USER`<br>`PIA_PASS` | Get token for API operations. |
 | [connect_wireguard.sh](connect_wireguard.sh) | `PIA_TOKEN`<br>`WG_SERVER_IP`<br>`WG_HOSTNAME` | Connect to Wireguard server obtained from get_region.sh. |
+| [port_forwarding.sh](port_forwarding.sh) | `WG_SERVER_IP`<br>`WG_HOSTNAME`<br>`PIA_TOKEN` | Enable port forwarding and bind port. Optional, `PAYLOAD_AND_SIGNATURE` to reuse existing port for keep-alive loop. |
 | [refresh_cacert.sh](refresh_cacert.sh) | | Download the latest CA certificate for PIA servers. |
---
+++ port_forwarding.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+## Port forwarding setup and keep-alive loop.
+# PAYLOAD_AND_SIGNATURE - optional, for keep-alive loop
+# KEEPALIVE=true - optional, enable keep-alive loop
+# KEEPALIVE_INT - optional, set keep-alive interval in seconds. default: 10m
+
+bind_port() {
+	local response="$(curl -sS --get \
+		--connect-to "$WG_HOSTNAME::$WG_SERVER_IP:" \
+		--cacert ca.rsa.4096.crt \
+		--data-urlencode "payload=$payload" \
+		--data-urlencode "signature=$signature" \
+		"https://${WG_HOSTNAME}:19999/bindPort")"
+
+	if [[ "$(echo "$response" | jq -r '.status')" != OK ]]; then
+		>&2 echo "$response"
+		exit 1
+	fi
+}
+
+check_interface() {
+	if ! wg show pia; then
+		>&2 echo 'wireguard interface not up'
+		exit 1
+	fi
+}
+
+cd "$(dirname "$0")"
+
+if [[ $(id -u) -ne 0 ]]; then
+	>&2 echo 'must be run as root'
+	exit 1
+fi
+
+check_interface
+
+# required vars
+WG_SERVER_IP="${WG_SERVER_IP:?missing WG_SERVER_IP}"
+WG_HOSTNAME="${WG_HOSTNAME:?missing WG_HOSTNAME}"
+PIA_TOKEN="${PIA_TOKEN:?missing PIA_TOKEN}"
+
+# optional vars
+KEEPALIVE="${KEEPALIVE:-}"
+KEEPALIVE_INT="${KEEPALIVE_INT:-'10m'}"
+
+if [[ -z "$PAYLOAD_AND_SIGNATURE" ]]; then
+	>&2 echo 'Getting new payload and signature...'
+	PAYLOAD_AND_SIGNATURE="$(curl -sS --get \
+		--connect-to "$WG_HOSTNAME::$WG_SERVER_IP:" \
+		--cacert ca.rsa.4096.crt \
+		--data-urlencode "token=$PIA_TOKEN"\
+		"https://${WG_HOSTNAME}:19999/getSignature")"
+else
+	>&2 echo 'Using existing payload and signature'
+fi
+
+PAYLOAD_AND_SIGNATURE=$(echo "$PAYLOAD_AND_SIGNATURE" | jq -cr)
+if [[ "$(echo "$PAYLOAD_AND_SIGNATURE" | jq -r '.status')" != OK ]]; then
+	>&2 echo "$PAYLOAD_AND_SIGNATURE"
+	exit 1
+fi
+
+signature=$(echo "$PAYLOAD_AND_SIGNATURE" | jq -r '.signature')
+payload=$(echo "$PAYLOAD_AND_SIGNATURE" | jq -r '.payload')
+port=$(echo "$payload" | base64 -d | jq -r '.port')
+expires_at=$(echo "$payload" | base64 -d | jq -r '.expires_at')
+
+if [[ "$KEEPALIVE" != true ]]; then
+	bind_port
+	cat <<-EOF
+		PAYLOAD_AND_SIGNATURE='$PAYLOAD_AND_SIGNATURE'
+		PORT_FORWARD_PORT=$port
+		EXPIRES_AT=$expires_at
+	EOF
+else
+	while true; do
+		echo -n 'Binding port forwarding port...'
+		bind_port
+		echo 'done.'
+		sleep "$KEEPALIVE_INT"
+	done
+fi
