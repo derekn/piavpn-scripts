a5a64c722194d654c2e87d36ed83429300c38573
Derek Nicol <1420397+derekn@users.noreply.github.com>
2022-03-01 10:18:36 -0600

    reuse env_vars file in setup if <24hrs

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@
 # required vars
 PIA_USER="${PIA_USER:?missing required var}"
 PIA_PASS="${PIA_PASS:?missing required var}"
+export PIA_USER PIA_PASS
+
+env_vars=/tmp/pia.env
 
 check_tool() {
 	if ! type $1 > /dev/null; then
@@ -22,20 +25,26 @@ for i in wg wg-quick curl jq; do
 	check_tool $i
 done
 
-export PIA_USER PIA_PASS
+cd "$(dirname "$0")"
+find "$env_vars" -mmin +1440 -delete &> /dev/null || true
 
-env_vars=/tmp/pia.env
-rm "$env_vars"
+if [[ -f "$env_vars" ]]; then
+	echo 'Loading existing variables...'
+	source "$env_vars"
+fi
 
-echo 'Getting region details...'
-./get_region.sh | tee -a "$env_vars"
-source "$env_vars"
+if [[ -z "$WG_SERVER_IP" || -z "$WG_HOSTNAME" ]]; then
+	echo 'Getting region details...'
+	./get_region.sh | tee -a "$env_vars"
+	source "$env_vars"
+fi
 export WG_SERVER_IP WG_HOSTNAME
 
-echo 'Getting API token...'
-./get_token.sh | tee -a "$env_vars"
-source "$env_vars"
+if [[ -z "$PIA_TOKEN" ]]; then
+	echo 'Getting API token...'
+	./get_token.sh >> "$env_vars"
+	source "$env_vars"
+fi
 export PIA_TOKEN
 
-echo 'Connecting VPN...'
 ./connect_wireguard.sh
